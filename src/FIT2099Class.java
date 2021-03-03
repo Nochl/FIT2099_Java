c9405a958d0cf147ccdd6e6998309146e7ee36a5
enoki123 <eleo0010@student.monash.edu>
2021-03-03 12:51:35 +1100

    Test

---
+++ src/FIT2099Class.java
@@ -0,0 +1,12 @@
+public class FIT2099Class {
+
+    public static void main(String[] args) {
+
+        SecondClass secondClass= new SecondClass();
+
+        secondClass.sayHello();
+
+        System.out.println("Hello World!!!");
+    }
+
+}
---
+++ src/SecondClass.java
@@ -0,0 +1,7 @@
+public class SecondClass {
+
+    public void sayHello(){
+        System.out.println("Hi from the second class");
+        return result;
+    }
+}
