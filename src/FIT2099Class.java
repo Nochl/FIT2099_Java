978bfd4dd779b3c92ae6dcf1508cf44a04703ab2
enoki123 <eleo0010@student.monash.edu>
2021-03-17 12:15:53 +1100

    Files from bootcamp 2 to continue

--- src/FIT2099Class.java
+++ src/FIT2099Class.java
@@ -2,7 +2,7 @@ public class FIT2099Class {
 
     public static void main(String[] args) {
 
-        SecondClass secondClass= new SecondClass();
+        SecondClass secondClass = new SecondClass();
 
         secondClass.sayHello();
 
