efe95e8543beabb1cfe1a9ee839154dd9b93bc5b
enoki123 <eleo0010@student.monash.edu>
2021-03-17 21:20:46 +1100

    Backup 1

--- Bootcamps/Bootcamp03/src/Car.java
+++ Bootcamps/Bootcamp03/src/Car.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public class Car {
+
+    ArrayList<Bid> bids = new ArrayList<Bid>();
 
     private String make = null;
     private String model = null;
@@ -7,6 +11,11 @@ public class Car {
         this.make = make;
         this.model = model;
     }
+
+    public addBid(Buyer newBuyer, int price, String date) {
+        newbuyer.
+    }
+
 
     public String description() {
         return "Maker:"+make+" and "+model;
