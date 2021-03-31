b82d2a1e4cfa0b30a028d4cef9bdd4f13a206930
enoki123 <eleo0010@student.monash.edu>
2021-03-31 14:54:54 +1100

    added true/false

--- Bootcamps/Bootcamp05/src/edu/monash/fit2099/bids/Bid.java
+++ Bootcamps/Bootcamp05/src/edu/monash/fit2099/bids/Bid.java
@@ -3,7 +3,7 @@ import edu.monash.fit2099.buyers.Buyer;
 
 public class Bid {
     private int bidId;
-    private Buyer buyer;
+    private int buyerid;
     private int price;
     private String date;
 
@@ -15,12 +15,12 @@ public class Bid {
         this.bidId = bidId;
     }
 
-    public Buyer getBuyer() {
-        return buyer;
+    public int getBuyer() {
+        return buyerid;
     }
 
-    public void setBuyer(Buyer buyer) {
-        this.buyer = buyer;
+    public void setBuyer(int buyerid) {
+        this.buyerid = buyerid;
     }
 
     public int getPrice() {
@@ -29,8 +29,8 @@ public class Bid {
 
     public boolean setPrice(int price) {
         boolean isValid=false;
-        if (price >= 0) {
-            isValid=true;
+        if(price >=0) {
+            isValid = true
             this.price = price;
         }
         return isValid;
@@ -40,18 +40,23 @@ public class Bid {
         return date;
     }
 
-    public void setDate(String date) {
-        boolean isValid=false
-        String[] val = date.split("/", 3);
-        day = int
-        if((val[0] )
-
-        this.date = date;
+    public boolean setDate(String date) {
+        boolean isValid=false;
+        String[] splits = date.split("/", 3);
+        int day = Integer.parseInt(splits[0]);
+        int month = Integer.parseInt(splits[1]);
+        int year = Integer.parseInt(splits[2]);
+        if ((day<32) && (day>0) && (month<13) && (month>0) && (year>1929) && (year<2022)) {
+            this.date = date;
+            isValid=true;
+        }
+        return isValid;
     }
 
-    public Bid(int bidId, Buyer buyer, int price, String date) {
+
+    public Bid(int bidId, int buyerid, int price, String date) {
         this.bidId = bidId;
-        this.buyer = buyer;
+        this.buyerid = buyerid;
         this.price = price;
         this.date = date;
     }
--- Bootcamps/Bootcamp05/src/edu/monash/fit2099/bids/BidsManager.java
+++ Bootcamps/Bootcamp05/src/edu/monash/fit2099/bids/BidsManager.java
@@ -1,17 +1,17 @@
 package edu.monash.fit2099.bids;
 
-import edu.monash.fit2099.buyers.Buyer;
-
 import java.util.HashMap;
 
 public class BidsManager {
     private int numBids = 0;
-    HashMap<Integer, Bid> bids= new HashMap<Integer,Bid>();
+    public HashMap<Integer, Bid> bids;
 
-
+    public BidsManager() {
+        bids = new HashMap<Integer,Bid>();
+    }
 
     public void addBid(int buyerId, int bidPrice, String bidDate) {
-        bids.put(buyerId, new Bid((numBids+1), new Buyer(buyerId), bidPrice, bidDate));
+        bids.put(buyerId, new Bid((numBids+1), buyerId, bidPrice, bidDate));
         numBids++;
 
     }
